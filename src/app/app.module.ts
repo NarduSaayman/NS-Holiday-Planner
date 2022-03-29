6f29509641967e3943f74670dddac9a8142fe238
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-03-29 13:43:15 +0200

    feat: :sparkles: Added initial required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { ItineraryComponent } from './components/itinerary/itinerary.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RegisterComponent,
+    LoginComponent,
+    ItineraryComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
